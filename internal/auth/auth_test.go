13fddce40a12c67b8696e2afa016a85d047175e9
dork-git <khatrimohit161@gmail.com>
2024-12-27 23:15:06 +0530

    add tests for internal auth package

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,51 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetApiKey(t *testing.T) {
+	tests := []struct {
+		name    string
+		headers http.Header
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "no auth header",
+			headers: http.Header{},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "malformed auth header",
+			headers: http.Header{
+				"Authorization": []string{"Bearer"},
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "valid auth header",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey 123"},
+			},
+			want:    "123",
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAPIKey(tt.headers)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetAPIKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
